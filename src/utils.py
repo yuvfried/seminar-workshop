bf3abbe86e1e88f850bc19f772a75b75518c0356
yuvalfrie <yuval.friedmann@mail.huji.ac.il>
2022-02-24 15:53:02 +0200

    =init

---
+++ src/utils.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+# String Representations for Plotting
+X_HAT_STR = "X\u0302"
+LAMBDA_STR = "\u03BB"
+ALPHA_STR = "\u03B1"
+BETA_STR = "\u03B2"
+
+
+def time_print(msg):
+    """
+Prints time stamp in format 'Year-month-day hour-minute-second and then msg'
+    :param msg:
+    """
+    print(datetime.now().strftime('%Y-%m-%d %X'), msg)
