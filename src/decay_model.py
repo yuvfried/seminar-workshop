44fe81e4aa9412961ab0eb375f2f08b9658cf796
Yuval Friedmann <yuvalfrie@cs.huji.ac.il>
2022-03-24 17:09:35 +0200

    fix halflife

--- src/decay_model.py
+++ src/decay_model.py
@@ -24,34 +24,33 @@ def get_regression_features(df_model, ma_halflife):
     features = pd.DataFrame.from_dict(out_cols).dropna()
     return features
 
-def fit_decay_to_signal(df_model, regularization_vals, auc_noise):
-    best_mse = np.inf
-    best_halflife = np.nan
-    best_lambda = np.nan
+def fit_decay_to_signal(df_model, halflife, regularization_vals, auc_noise):
     y = simulate_auc(df_model["S"], sigma=auc_noise).iloc[1:]   # we need t-1 for making explained variables
-    for h in tqdm(halflife_vals, desc="halflife", leave=False):
-        features = get_regression_features(df_model, ma_halflife=h)
-        param_grid = {'alpha':regularization_vals}
-        cv_search = GridSearchCV(Ridge(random_state=123, solver='svd'), param_grid, cv=5, 
-            scoring='neg_mean_squared_error', n_jobs=-1, verbose=False)
-        cv_search.fit(features, y)
-        best_lambda_mse = cv_search.cv_results_['mean_test_score'][cv_search.best_index_] * (-1)    # sklearn implementation of scoring
-        lambda_candidate = cv_search.best_params_["alpha"]
-        if best_lambda_mse < best_mse:
-            best_mse = best_lambda_mse
-            best_halflife = h
-            best_lambda = lambda_candidate
-    return best_halflife, best_lambda
+    features = get_regression_features(df_model, ma_halflife=halflife)  # the model regression variables
+    param_grid = {'alpha':regularization_vals}  # search space of different lambda's
+    
+    # fir regression for each lambda
+    cv_search = GridSearchCV(Ridge(random_state=123, solver='svd'), param_grid, cv=5, 
+        scoring='neg_mean_squared_error', n_jobs=-1, verbose=False)
+    cv_search.fit(features, y)
+
+    best_lambda = cv_search.best_params_["alpha"]
+    return best_lambda
 
 def convert_IB_to_decay(model_surprise_dict, regularization_vals, auc_noise=0.0):
     # TODO: allow saving to file
+    n_to_halflife_map = pd.read_json("data/N_halflife_map.json", typ="series")
     d_results = dict()
     for n in tqdm(Ns, desc = 'N'):
         d_results[n] = {}
         for beta_ind in tqdm(range(len(betas)), desc=BETA_STR, leave=False):
             df_model = model_surprise_dict[n, beta_ind]
+            h = n_to_halflife_map.loc[n].item()
             d_results[n][beta_ind] = fit_decay_to_signal(
-                df_model.dropna(), regularization_vals, auc_noise)
+                df_model.dropna(), 
+                halflife=h, 
+                regularization_vals=regularization_vals, 
+                auc_noise= auc_noise)
     
     return d_results
 
