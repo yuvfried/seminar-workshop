8456f86828711c7f58d3213c3d7fe9c2d822476c
Yuval Friedmann <yuvalfrie@cs.huji.ac.il>
2022-03-25 13:08:54 +0300

    noise vals

--- src/config.py
+++ src/config.py
@@ -9,3 +9,6 @@ betas = np.logspace(0, 2, num=20, base=10)
 # Regression
 halflife_vals = Ns
 regularization_vals = np.linspace(0, 2, num=20)
+
+# noises
+noises = np.linspace(0,1, num=11).tolist() + [1.5,2,3,5]
